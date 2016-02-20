6e241119d8d608789cc246bb5a975e6b1e151950
Luke Platz <=>
2016-02-20 14:42:44 -0500

    pushing other changes

--- lighting_sample/lighting_sample.ino
+++ lighting_sample/lighting_sample.ino
@@ -10,63 +10,36 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(150, PIN, NEO_GRB + NEO_KHZ800);
-int currentColor = 0;
-float voltage = 0.0;
-int count = 0;
 
 void setup() {
-  Serial.begin(9600); //bits per second
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
-  
 }
 
-void loop() {
-  count++;
-  //----Pressure Sensor----
-    // Read the input on analog pin 0:
-    int sensorValue = analogRead(A0);
-    // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-    if (count >= 50){
-      voltage = sensorValue * (5.0 / 1023.0);
-      count = 0;
-    }
-    
-    if(4.5 <= voltage && voltage < 4.9 && currentColor != 2){
-      colorWipe(strip.Color(0, 255, 0), 15); // Green
-      currentColor = 2;
-    }else if(voltage >= 4.9 && currentColor != 1){
-      colorWipe(strip.Color(0, 0, 255), 15); // Blue
-      currentColor = 1;
-    }else if (voltage < 4.5 && currentColor != 0) {
-      colorWipe(strip.Color(255, 0, 0), 15); // Red
-      currentColor = 0;
-    }
-  //----Lights----
-  // Some example procedures showing how to display to the pixels:
-//  colorWipe(strip.Color(255, 0, 0), 15); // Red
-//  colorWipe(strip.Color(0, 255, 0), 15); // Green
-//  colorWipe(strip.Color(0, 0, 255), 15); // Blue
-//
-//  // Send a theater pixel chase in...
-//  theaterChase(strip.Color(127, 127, 127), 15); // White
-//  theaterChase(strip.Color(127,   0,   0), 15); // Red
-//  theaterChase(strip.Color(  0,   0, 127), 15); // Blue
-//
-//  rainbow(15);
-//  rainbowCycle(15);
-//  theaterChaseRainbow(25);
-//  delay(1000);
+void loop() {    
+    //----Lights----
+    // Some example procedures showing how to display to the pixels:
+    colorWipe(strip.Color(255, 0, 0), 15); // Red
+    colorWipe(strip.Color(0, 255, 0), 15); // Green
+    colorWipe(strip.Color(0, 0, 255), 15); // Blue
+  
+    // Send a theater pixel chase in...
+    theaterChase(strip.Color(127, 127, 127), 15); // White
+    theaterChase(strip.Color(127,   0,   0), 15); // Red
+    theaterChase(strip.Color(  0,   0, 127), 15); // Blue
+  
+    rainbow(15);
+    rainbowCycle(15);
+    theaterChaseRainbow(25);
 }
 
 // Fill the dots one after the other with a color
 void colorWipe(uint32_t c, uint8_t wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
       strip.setPixelColor(i, c);
-//      strip.show();
-//      delay(wait);
+      strip.show();
+      delay(wait);
   }
-  strip.show();
 }
 
 void rainbow(uint8_t wait) {
